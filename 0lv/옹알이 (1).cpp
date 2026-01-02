f89efd9d1a80022e5e80c883b0750425188611cf
oneyoung102 <146638457+oneyoung102@users.noreply.github.com>
2026-01-02 16:56:05 +0900

    Create 옹알이 (1).cpp

---
+++ 0lv/옹알이 (1).cpp
@@ -0,0 +1,32 @@
+//2024-12-11 14:09:37
+//https://school.programmers.co.kr/learn/courses/30/lessons/120956
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    for(auto& it : babbling)
+    {
+        int pos = 0;
+        while(pos < it.size())
+        {
+            if(it.substr(pos,3) == "aya")
+                pos+=3;
+            else if(it.substr(pos,3) == "woo")
+                pos+=3;
+            else if(it.substr(pos,2) == "ye")
+                pos+=2;
+            else if(it.substr(pos,2) == "ma")
+                pos+=2;
+            else
+                break;
+        }   
+        if(pos == it.size())
+            answer++;
+    }
+    return answer;
+}
