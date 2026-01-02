66d9c7b8e94833d74eb4870ee90c956394ae6b8e
oneyoung102 <146638457+oneyoung102@users.noreply.github.com>
2026-01-02 17:04:53 +0900

    Create [PCCE 기출문제] 1번  문자 출력.cpp

---
+++ 0lv/[PCCE 기출문제] 1번  문자 출력.cpp
@@ -0,0 +1,15 @@
+//2024-12-11 13:23:07
+//https://school.programmers.co.kr/learn/courses/30/lessons/340207
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "";
+
+    cout << "321" << endl;
+    cout << message << endl;
+
+    return 0;
+}
