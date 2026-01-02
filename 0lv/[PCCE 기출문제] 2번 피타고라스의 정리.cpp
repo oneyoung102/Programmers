3ce999d28301cac071a39c933e6020406a968d7b
oneyoung102 <146638457+oneyoung102@users.noreply.github.com>
2026-01-02 17:07:02 +0900

    Create [PCCE 기출문제] 2번 피타고라스의 정리.cpp

---
+++ 0lv/[PCCE 기출문제] 2번 피타고라스의 정리.cpp
@@ -0,0 +1,16 @@
+//2024-12-11 13:25:39
+//https://school.programmers.co.kr/learn/courses/30/lessons/250132
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int c;
+    cin >> a >> c;
+    
+    int b_square = c*c - a*a;
+    cout << b_square << endl;
+    return 0;
+}
