966bc3fdb80f938e8cdb9153c9225beacd3e6a67
oneyoung102 <146638457+oneyoung102@users.noreply.github.com>
2026-01-06 10:03:20 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b, int c, int d) {
+    int answer = 0;
+    int amount[7]={0,0,0,0,0,0,0};
+    amount[a]++;amount[b]++;amount[c]++;amount[d]++;
+    for(int i=1;i<7;i++)
+    {
+        if(amount[i]==4)
+        {
+            answer=1111*i;
+            break;
+        }
+        else if(amount[i]==3)
+        {
+            for(int j=1;j<7;j++)
+            {
+                if(amount[j]==1)
+                {
+                    answer=(10*i+j)*(10*i+j);
+                    break;
+                }
+            }  
+            break;
+        }
+        else if(amount[i]==2)
+        {
+        for(int j=1;j<7;j++)
+        {
+                if(amount[j]==2&&j!=i)
+                {
+                    answer=(i+j)*abs(i-j);
+                }
+                else if(amount[j]==1)
+                {
+                    for(int k=1+j;k<7;k++)
+                    {
+                        if(amount[k]==1)
+                        {
+                            answer=k*j; 
+                            break;
+                        }
+                    }
+                }
+         }
+            break;
+        }
+        else if(amount[i]==1)
+        {
+            int mini=7;
+            for(int j=1;j<7;j++)
+            {             
+                if(amount[j]==1)
+                {
+                     if(mini>j)
+                         mini=j;
+                }
+                else if(amount[j]>1)
+                {
+                    mini=7;
+                    break;
+                }
+            }
+            if(mini!=7)
+            {
+                 answer=mini;
+                 break;
+            }  
+        }
+    }
+    
+    return answer;
+}
