e7a1d3c635a6c7ce0d9aa6864143bd2d2a2bead6
oneyoung102 <146638457+oneyoung102@users.noreply.github.com>
2026-01-14 20:51:02 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 3.58 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 1;
+    while((a+1)/2 != (b+1)/2)
+    {
+        a = (a+1)/2;
+        b = (b+1)/2;
+        ++answer;
+    }
+    return answer;
+}
